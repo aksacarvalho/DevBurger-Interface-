ccfebbb5bfbffa558928d16a1f14e61ff6486c13
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-07-03 16:41:02 -0300

    iniciando a tela de login

--- src/containers/login/index.jsx
+++ src/containers/login/index.jsx
@@ -1,11 +1,42 @@
 
+import Logo from '../../assets/logo.svg'
+
+import { Container, 
+  Form, 
+  InputContainer, 
+  LeftContainer, 
+  RightContainer, 
+  Title, 
+  Link,
+  Button, } from './styles'
 
 export function Login() {
 
    return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="logo-devburger" />
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+         Olá, seja bem vindo ao <span>Dev Burguer!</span> 
+Acesse com seu <span>login e senha.</span>
+        </Title>
+       <Form action="">
+         <InputContainer>
+             <label>Email</label>
+             <input type="email" />
+         </InputContainer>
+
+         <InputContainer>
+             <label>Senha</label>
+             <input type="password" />
+         </InputContainer>
+         <Link>Esqueci minha senha.</Link> 
+         <Button>Entrar</Button>
+        </Form>
+       </RightContainer> 
+    </Container>
 
    );
 }

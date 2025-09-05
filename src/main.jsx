2c19dffe7be7fde904008a4559d8d9966a96c769
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-09-05 14:58:20 -0300

    Criando o usuário

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 
-import { Login } from './containers/login'
+import { router } from './routes';
 import GlobalStyles from './styles/globalStyles'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+
     <GlobalStyles  />
+    <RouterProvider router={router}/>
     <ToastContainer autoClose={2000} theme="colored" />
    </StrictMode>,
 )

ed53941154b1aaa0991b719fb3942354e5739765
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-10-30 16:57:00 -0300

    Utilizando Outlet

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
-
-import { router } from './routes';
 import GlobalStyles from './styles/globalStyles'
 import AppProvider from './hooks';
 import { Elements } from '@stripe/react-stripe-js';
@@ -12,13 +10,16 @@ import stripePromise from './config/stripeConfig';
 import { ThemeProvider } from 'styled-components';
 
 import { standardTheme } from './styles/themes/standard';
+import { Router } from './routes';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={standardTheme}>
     <AppProvider>
       <Elements stripe={stripePromise}>
-    <RouterProvider router={router}/>
+     <BrowserRouter>
+      <Router />
+     </BrowserRouter>
     </Elements>
      <GlobalStyles  />
     <ToastContainer autoClose={2000} theme="colored" />

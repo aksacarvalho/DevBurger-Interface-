fb0d8eaa13753e05722e2f47cc7054d20d7ab09c
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-11-10 14:54:15 -0300

    Aula5 - Criando a tela de adcionar produtos

--- src/containers/Admin/NewProduct/index.jsx
+++ src/containers/Admin/NewProduct/index.jsx
@@ -1,7 +1,63 @@
+import { useForm } from "react-hook-form"
+import {  ImageBrokenIcon } from '@phosphor-icons/react'
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+import {
+  Container,
+  Form,
+  Input,
+  InputGroup,
+  Label,
+  LabelUpload,
+  Select,
+  SubmitButton,
+} from './styles';
+
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    age: yup.number().positive().integer().required(),
+  })
+  .required()
+
 export function NewProduct() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+  const onSubmit = (data) => console.log(data)
+
   return (
-    <div>
-       <h2>NewProduct</h2>
-    </div>
+     <Container>
+       <Form>
+         <InputGroup>
+          <Label>Nome</Label>
+           <Input />
+         </InputGroup>
+
+         <InputGroup>
+          <Label>Preço</Label>
+           <Input />
+         </InputGroup>
+
+          <InputGroup>
+           <LabelUpload>
+            <ImageBrokenIcon />
+              <input type="file"/>
+           </LabelUpload>
+          </InputGroup>
+
+          <InputGroup>
+           <Label>Categoria</Label>
+            <Select />
+          </InputGroup>
+
+          <SubmitButton>Adicionar Produto</SubmitButton>
+       </Form>
+     </Container>
   );
 }
